2894110cb786d314a51e5423534bb1e31fd6416b
mbobic1 <mbobic1@etf.unsa.ba>
2022-11-27 00:21:05 +0100

    uradjen prvi zadatak

--- scripts/TabelaPrisustvo.js
+++ scripts/TabelaPrisustvo.js
@@ -79,21 +79,169 @@ let TabelaPrisustvo = function (divRef, podaci) {
     //Postoji sedmica, između dvije sedmice za koje je uneseno prisustvo bar 
     //jednom studentu, u kojoj nema unesenog prisustva. Npr. uneseno je 
     //prisustvo za sedmice 1 i 3 ali nijedan student nema prisustvo za sedmicu 2
-    for(var i =0; i<podaci.prisustva.length; i++){
-        for(var j=i+1; j<podaci.prisustva.length; j++){
-            
+    /*for(var i =0; i<podaci.prisustva.length; i++){
+        for(var j=0; j<podaci.studenti.length; j++){
+            if(podaci.prisustva[i].index==podaci.studenti[j].index){
+
+            }
+        }
+    }*/
+
+    function promjeniBroj(broj){
+        if(broj==1){
+            return "I";
+        }
+        else if(broj==2){
+            return "II";
+        }
+        else if(broj==3){
+            return "III";
+        }
+        else if(broj==4){
+            return "IV";
+        }
+        else if(broj==5){
+            return "V";
+        }
+        else if(broj==6){
+            return "VI";
+        }
+        else if(broj==7){
+            return "VII";
+        }
+        else if(broj==8){
+            return "VIII";
+        }
+        else if(broj==9){
+            return "IX";
+        }
+        else if(broj==10){
+            return "X";
+        }
+        else if(broj==11){
+            return "XI";
+        }
+        else if(broj==12){
+            return "XII";
+        }
+        else if(broj==13){
+            return "XIII";
+        }
+        else if(broj==14){
+            return "XIV";
+        }
+        else if(broj==15){
+            return "XV";
         }
     }
-
-
-
-
-    var container = document.getElementById('divSadrzaj');
-    var myhtml='';
-    for(var i=0; i < podaci.studenti.length; i++){
-        myhtml += '<p>' + podaci.studenti[i].ime +'</p>';
+    var ukupnosedmica=0;
+    for(var i=0; i<podaci.prisustva.length; i++){
+        if(podaci.prisustva[i].index==podaci.studenti[0].index){
+        ukupnosedmica++;
+        }
     }
-    divRef.innerHTML = myhtml;
+    var uzmiIndekse = [];
+    var indekspostoji=0;
+    var ukupnoVjIPr = podaci.brojPredavanjaSedmicno+podaci.brojVjezbiSedmicno;
+    //pocinje tabela 
+    var tabela1 = "<h1><b> Predmet: "+ podaci.predmet + "</b></h1>";
+    tabela1 += "<table class=\"table\"><thead><tr><th>Ime i Prezima </th><th>Index</th>";
+    for(var i=0; i<ukupnosedmica; i++){
+        if(i==ukupnosedmica-1){
+            i+=1;
+            tabela1+= "<td colspan=\""+ukupnoVjIPr+"\"><b>"+promjeniBroj(i)+"</b></td>";
+            i-=1;
+        }
+        else{
+            i+=1;
+            tabela1+= "<th><b>"+promjeniBroj(i)+"</b></th>"
+            i-=1;
+        }       
+    }
+    if(ukupnosedmica<15){
+        ukupnosedmica++;
+        tabela1+= "<th><b>" + promjeniBroj(ukupnosedmica) + "-XV</b></th>";
+    }
+    tabela1+="</tr> </thead>";
+    //tbodypocinje
+    tabela1+="<tbody>"
+    var izasaoPrIVj=0;
+    for(var i = 0; i< podaci.studenti.length; i++){
+        tabela1+="<tr>";
+        indekspostoji=0;
+            for(var j=0; j<podaci.prisustva.length; j++){
+                izasaoPrIVj=0;
+                indekspostoji=0;
+                if(podaci.prisustva[j].index==podaci.studenti[i].index){
+                    for(var k=0; k<uzmiIndekse.length; k++){
+                        if(uzmiIndekse[k]==podaci.studenti[i].index){
+                            indekspostoji=1;
+                            continue;
+                        }
+                    }
+                    izasaoPrIVj+=(podaci.prisustva[j].predavanja + podaci.prisustva[j].vjezbe);
+                    if(indekspostoji==0){
+                        uzmiIndekse.push(podaci.studenti[j].index);
+                        tabela1+="<td rowspan=\"2\">"+podaci.studenti[j].ime+"</td>";
+                        tabela1+="<td rowspan=\"2\">"+podaci.studenti[j].index+"</td>";
+                    }
+                    console.log("Ovdje sedmica:");
+                    console.log(ukupnosedmica);
+                    if(podaci.prisustva[j].sedmica==(ukupnosedmica-1)){ 
+                        console.log(ukupnosedmica);  
+                        for(var l=0; l<podaci.brojPredavanjaSedmicno; l++){
+                            l+=1;
+                            tabela1+="<td> p <br>"+ l + "</td>"; 
+                            l-=1;
+                        }
+                        for(var l =0; l<podaci.brojVjezbiSedmicno; l++){
+                            l+=1;
+                            tabela1+="<td> v <br>"+ l + "</td>";
+                            l-=1;
+                        }
+                    }
+                    else{
+                        tabela1+="<td rowspan=\"2\">"+ ((izasaoPrIVj/ukupnoVjIPr)*100) +"%</td>";
+                    }
+                }            
+            }
+            tabela1+="<td rowspan=\"2\"></td>"; 
+            //red dupli sad pravimo   
+            var brojPred = 0; 
+            var brojVjez = 0;        
+            tabela1+="<tr>";
+            for(var j=0; j<podaci.prisustva.length; j++){              
+                if(podaci.prisustva[j].index==podaci.studenti[i].index){
+                    if(podaci.prisustva[j].sedmica==ukupnosedmica-1){
+                        brojPred=podaci.prisustva[j].predavanja;
+                        for(var m = 0; m<podaci.brojPredavanjaSedmicno; m++){
+                            if(m<=brojPred-1){
+                                tabela1+="<td class=\"zelena\"></td>"
+                            }
+                            else{
+                                tabela1+="<td class=\"crvena\"></td>"
+                            }
+                        }
+                        brojVjez=podaci.prisustva[j].vjezbe;
+                        for(var m =0; m<podaci.brojVjezbiSedmicno; m++){
+                            if(m<=brojVjez-1){
+                                tabela1+="<td class=\"zelena\"></td>"
+                            }
+                            else{
+                                tabela1+="<td class=\"crvena\"></td>"
+                            }
+                        }
+                    }
+                }             
+            }
+            tabela1+="</tr>";
+            //zavrsava red novi for pocinje
+        tabela1+="</tr>";
+    }
+    tabela1+="</tbody></table>";
+    divRef.innerHTML = tabela1;
+    //zavrsava tabela
+
 
 
 
